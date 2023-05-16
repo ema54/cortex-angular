875961b96d4b9174153fa0387d8b91de3e08d0ef
Ema Kotorac <emakotorac@gmail.com>
2023-05-16 17:16:59 +0200

    Updated code Databinding 16/05/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent,
     ServerComponent,
-    ServersComponent,
-   
+    ServersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-   
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
-  serverId=10;
-  serverStatus="offline";
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 
-  getServerStatus(){
+  getServerStatus() {
     return this.serverStatus;
   }
 }
