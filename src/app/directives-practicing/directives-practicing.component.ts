7cbb42df9afea6f0f4859fb37b576a6f6e53ac36
Ema Kotorac <emakotorac@gmail.com>
2023-05-17 17:50:16 +0200

    Homework-3

--- src/app/directives-practicing/directives-practicing.component.ts
+++ src/app/directives-practicing/directives-practicing.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./directives-practicing.component.css']
 })
 export class DirectivesPracticingComponent {
+    logEntries: number[] = [];
+    showParagraph = false;
+  
+    onClick(): void {
+      this.showParagraph = true;
+      const logEntry = this.logEntries.length + 1;
+      this.logEntries.push(logEntry);
+      console.log(logEntry);
+    }
+  
+  }
 
-}
+
